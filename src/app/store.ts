1b4aa73ebad8eaef71706601531fa284dca6fed6
Chathumina <chathumina.dev@gmail.com>
2025-02-02 21:32:25 +0530

    setup add products page

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,15 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "../features/auth/authApi";
 import { userApi } from "../features/user/userApi";
 import authReducer from "../features/auth/authSlice";
+import { productApi } from "../features/product/productApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, userApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      userApi.middleware,
+      productApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
