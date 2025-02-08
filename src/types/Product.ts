8af9773026efeb5e53f1123975853e7b670ee05b
Chathumina <chathumina.dev@gmail.com>
2025-02-08 18:50:50 +0530

    update product.ts

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,8 +1,36 @@
+export enum ToyCategory {
+  Educational = "Educational Toys",
+  ActionFigures = "Action Figures & Collectibles",
+  Dolls = "Dolls & Dollhouses",
+  BoardGames = "Board Games & Puzzles",
+  Outdoor = "Outdoor & Sports Toys",
+  Electronic = "Electronic & Remote-Controlled Toys",
+  Plushies = "Soft Toys & Plushies",
+  ArtsCrafts = "Arts & Crafts",
+  Musical = "Musical Toys",
+  BabyToddler = "Baby & Toddler Toys",
+}
+
+export interface ProductData {
+  productCode: string;
+  name: string;
+  description: string;
+  price: number;
+  mainImage: string;
+  subImages: string[];
+  category: ToyCategory;
+  isNewProduct: boolean;
+  discount: number;
+  stock: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface ProductCart {
-    imageUrl: string;
-    productCode: number;
-    title: string;
-    price: number;
-    quantity: number;
-    color: string;
+  imageUrl: string;
+  productCode: number;
+  title: string;
+  price: number;
+  quantity: number;
+  color: string;
 }
