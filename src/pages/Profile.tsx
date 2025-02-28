d92de0c1f8818832636bfc78283157ac0661996d
Chathumina <chathumina.dev@gmail.com>
2025-02-28 22:39:29 +0530

    update user profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,31 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProfileField from "../components/ProfileField";
 import Button from "../components/Button";
 import ProfileSidebar from "../components/ProfileSideBar";
 import MobileProfileMenu from "../components/MobileProfileMenu";
 import AccountTable from "../components/AccountTable";
 import { Address } from "../components/Address";
+import { useFetchUserQuery } from "../features/user/userApi";
+import Loader from "../components/Loader";
 
 const ProfilePage: React.FC = () => {
-  const [formData, setFormData] = useState({
+  const [userFormData, setUserFormData] = useState({
     firstName: "",
     lastName: "",
     displayName: "",
     email: "",
+  });
+
+  const [passwordFormData, setPasswordFormData] = useState({
     oldPassword: "",
     newPassword: "",
     repeatPassword: "",
   });
 
   const [activeSection, setActiveSection] = useState("Account");
+  const { data: user, isLoading, refetch } = useFetchUserQuery(undefined);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  useEffect(() => {
+    if (user) {
+      setUserFormData({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        displayName: user.username,
+        email: user.email,
+      });
+    }
+  }, [user]);
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement> | string,
+    name?: string
+  ) => {
+    if (typeof event === "string" && name) {
+      setUserFormData((prev) => ({ ...prev, [name]: event }));
+    } else if (typeof event !== "string") {
+      setUserFormData((prev) => ({
+        ...prev,
+        [event.target.name]: event.target.value,
+      }));
+    }
   };
 
   const handleSaveChanges = () => {
-    if (formData.newPassword !== formData.repeatPassword) {
+    if (passwordFormData.newPassword !== passwordFormData.repeatPassword) {
       alert("Passwords do not match!");
       return;
     }
@@ -41,7 +67,11 @@ const ProfilePage: React.FC = () => {
   };
 
   const userName =
-    `${formData.firstName} ${formData.lastName}`.trim() || "Guest";
+    `${userFormData.firstName} ${userFormData.lastName}`.trim() || "Guest";
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <div className="min-h-screen flex flex-col px-4 md:px-20">
@@ -72,72 +102,77 @@ const ProfilePage: React.FC = () => {
             </h2>
 
             {activeSection === "Account" && (
-              <div className="space-y-5">
-                <ProfileField
-                  name="firstName"
-                  label="First Name"
-                  value={formData.firstName}
-                  onChange={handleInputChange}
-                />
+              <>
+                <div className="space-y-5">
+                  <form action="">
+                    <ProfileField
+                      name="firstName"
+                      label="First Name"
+                      value={userFormData.firstName}
+                      onChange={handleChange}
+                    />
+                    <ProfileField
+                      name="lastName"
+                      label="Last Name"
+                      value={userFormData.lastName}
+                      onChange={handleChange}
+                    />
+                    <ProfileField
+                      name="displayName"
+                      label="Display Name"
+                      value={userFormData.displayName}
+                      onChange={handleChange}
+                    />
+                    <p className="text-sm text-gray-500">
+                      This will be how your name will be displayed in the
+                      account section and in reviews.
+                    </p>
+                    <ProfileField
+                      name="email"
+                      label="Email"
+                      value={userFormData.email}
+                      onChange={handleChange}
+                    />
+                    <Button
+                      type="button"
+                      label="Save Changes"
+                      onClick={handleSaveChanges}
+                      className="bg-black text-white mt-2 w-full "
+                    />
+                  </form>
 
-                <ProfileField
-                  name="lastName"
-                  label="Last Name"
-                  value={formData.lastName}
-                  onChange={handleInputChange}
-                />
-
-                <ProfileField
-                  name="displayName"
-                  label="Display Name"
-                  value={formData.displayName}
-                  onChange={handleInputChange}
-                />
-                <p className="text-sm text-gray-500">
-                  This will be how your name will be displayed in the account
-                  section and in reviews.
-                </p>
-
-                <ProfileField
-                  name="email"
-                  label="Email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-
-                <h2 className="text-xl font-semibold mt-8">Password</h2>
-
-                <ProfileField
-                  name="oldPassword"
-                  label="Old Password"
-                  type="password"
-                  value={formData.oldPassword}
-                  onChange={handleInputChange}
-                />
-
-                <ProfileField
-                  name="newPassword"
-                  label="New Password"
-                  type="password"
-                  value={formData.newPassword}
-                  onChange={handleInputChange}
-                />
-
-                <ProfileField
-                  name="repeatPassword"
-                  label="Repeat New Password"
-                  type="password"
-                  value={formData.repeatPassword}
-                  onChange={handleInputChange}
-                />
-
-                <Button
-                  type="button"
-                  label="Save Changes"
-                  onClick={handleSaveChanges}
-                  className="bg-black text-white mt-4 w-full "
-                />
-              </div>
+                  <form action="">
+                    <h2 className="text-xl font-semibold mt-8">Password</h2>
+                    <ProfileField
+                      name="oldPassword"
+                      label="Old Password"
+                      type="password"
+                      value={passwordFormData.oldPassword}
+                      onChange={handleChange}
+                    />
+                    <ProfileField
+                      name="newPassword"
+                      label="New Password"
+                      type="password"
+                      value={passwordFormData.newPassword}
+                      onChange={handleChange}
+                    />
+                    <ProfileField
+                      name="repeatPassword"
+                      label="Repeat New Password"
+                      type="password"
+                      value={passwordFormData.repeatPassword}
+                      onChange={handleChange}
+                    />
+                    <Button
+                      type="button"
+                      label="Change Password"
+                      onClick={handleSaveChanges}
+                      className="bg-black text-white mt-4 w-full "
+                    />
+                  </form>
+                </div>
+              </>
             )}
             {activeSection === "Address" && (
               <div>
