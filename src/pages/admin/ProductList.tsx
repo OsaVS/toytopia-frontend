8dadc00f2baaef10a46f8edb944cf7c7a79a8489
OsaVS <41975253+OsaVS@users.noreply.github.com>
2025-03-14 01:55:24 +0530

    Admin product table

--- src/pages/admin/ProductList.tsx
+++ src/pages/admin/ProductList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useGetAllProductsQuery } from "../../features/product/productApi";
 import Loader from "../../components/Loader";
+import { ProductData } from "../../types/product";
 
 const ProductList = () => {
   const { data, isLoading, refetch } = useGetAllProductsQuery(undefined);
@@ -9,7 +10,33 @@ const ProductList = () => {
     return <Loader />;
   }
 
-  return <div>ProductList</div>;
+  return (
+  <div className="container mx-auto p-4">
+    <h1 className="text-2xl font-semibold mb-5">Products List</h1>
+    <table className="w-full border">
+      <thead>
+        <tr className="bg-gray-100">
+          <th className="border px-4 py-2">Product Code</th>
+          <th className="border px-4 py-2">Name</th>
+          <th className="border px-4 py-2">Category</th>
+          <th className="border px-4 py-2">Price</th>
+          <th className="border px-4 py-2">Stock</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.data.map((product: ProductData) => (
+          <tr key={product._id}>
+            <td className="border px-4 py-2">{product._id}</td>
+            <td className="border px-4 py-2">{product.name}</td>
+            <td className="border px-4 py-2">{product.category}</td>
+            <td className="border px-4 py-2">{product.price}</td>
+            <td className="border px-4 py-2">{product.stock}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+  </div>);
 };
 
 export default ProductList;
